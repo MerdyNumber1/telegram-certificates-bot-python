118a7dc431b63aed9b48ed011b2047194f61021f
merdy <sampekmoy@gmail.com>
2020-12-11 23:12:46 +0300

    fix too long values

--- src/handlers/create_handler.py
+++ src/handlers/create_handler.py
@@ -1,5 +1,5 @@
 from services.CertificateService import CertificateService
-from peewee import DoesNotExist
+from peewee import DataError
 import datetime
 
 CertificateService = CertificateService()
@@ -12,14 +12,18 @@ def create_handler(update, context):
         title = context.args[0]
         recipient = context.args[1]
 
-        days_to_expire = None
-        if context.args[2].isdigit():
-            days_to_expire = int(context.args[2])
+        if len(title) < 256 and len(recipient) < 256:
 
-        certificate = CertificateService.create_certificate(title=title,
-                                                            recipient_name=recipient,
-                                                            days_to_expire=days_to_expire)
+            days_to_expire = None
+            if context.args[2].isdigit():
+                days_to_expire = int(context.args[2])
 
-        message = f"Certificate created: \n{repr(certificate)}"
+            certificate = CertificateService.create_certificate(title=title,
+                                                                recipient_name=recipient,
+                                                                days_to_expire=days_to_expire)
+            message = f"Certificate created: \n{repr(certificate)}"
+
+        else:
+            message = "Data too long"
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
